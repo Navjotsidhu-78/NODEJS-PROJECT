fae8a9c8b6f54e149d98097ce80ce9807615bdfc
Navjotsidhu-78 <nnavjotkaur03@gmail.com>
2024-06-23 19:05:19 -0700

    created EJS for Add new book and created route to handle it.

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require("path");
 const mongodb = require('mongodb');
+const methodOverride= require("method-override");
 require('dotenv').config();
 
 const app =express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+
 const port = process.env.PORT || 3000;
 const mongoURI = process.env.MONGO_URI;
 
+let db; // mongoDB connection reference
 
-let db;
-
+// Connect to MongoDB
 mongodb.MongoClient.connect(mongoURI)
 .then((client)=>{
     console.log("MongoDB connected");
     db = client.db("library_management_system");
 })
 .catch((err) => {
-    HTMLFormControlsCollection.error("MongoDB connection error: ", err);
+    console.error("MongoDB connection error: ", err);
     process.exit(1);
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get('/', (req, res) => {
+    res.send("Hello World");
 });
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+app.get("/books/new", (req, res) => {
+    res.render("add_book");
+})
+
+app.post("/books",(req, res) =>{
+    const book ={
+        title: req.body.title,
+        author: req.body.author,
+        publishedDate: new Date(req.body.publishedDate)
+    };
+
+    db.collection("books").insertOne(book)
+    .then(result => res.send(result))
+    .catch(error => res.status(500).json({ error: "An error occured while inserting book", details: err}));
 });
+
+app.listen(port, () => {                                
+    console.log(`Server started at http://localhost:${port}`); 
+});        
